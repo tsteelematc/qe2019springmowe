5f32373bb272371f878f108477284045b9efdadb
= <=>
2019-04-01 15:25:46 -0500

    updated angular.json for bootstrap

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,21 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
-  title = 'quiz-editor';
+  title       = 'quiz-editor';
+  myWidth     = 250;
+  get titleColor() {
+    return this.myWidth > 250 ? 'pink':'black';
+  }
+
+  increaseWidth = () =>
+  {
+    this.myWidth *= 1.5;
+  }
+
+  decreaseWidth = () =>
+  {
+    this.myWidth /= 1.5;
+  }
 }
