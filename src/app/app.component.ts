ba5c50b3ac9e8a15e81e9f0e7e9bbd6209dd117d
tsteelematc <tsteele@madisoncollege.edu>
2019-04-24 14:55:40 -0500

    Cancel batch edits working...
    Stupid quiz-editor almost done ! ! !

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -59,23 +59,7 @@ export class AppComponent implements OnInit {
   errorCallingRestEndpoint = false;
 
   ngOnInit() {
-    this.quizSvc.getQuizzes().subscribe(
-      (data) => {
-        console.log(data);
-        this.quizzes = (<any[]> data).map(x => ({
-          name: x.name
-          , originalName: x.name
-          , questions: x.questions
-          , originalQuestionsChecksum: x.questions.map(x => x.name).join('~')
-          , markedForDelete: false
-        }));
-      }
-
-      , (error) => {
-        console.log(error);
-        this.errorCallingRestEndpoint = true;
-      }
-    );
+    this.loadAllQuizzes();
   }
 
   title = 'quiz-editor';
@@ -84,6 +68,27 @@ export class AppComponent implements OnInit {
 
   quizzes: QuizDisplay[] = [];
   selectedQuiz: QuizDisplay = undefined;
+
+  cancelBatchEdits() {
+    this.loadAllQuizzes();
+    this.setSelectedQuiz(undefined);
+  }
+
+  private loadAllQuizzes() {
+    this.quizSvc.getQuizzes().subscribe((data) => {
+      console.log(data);
+      this.quizzes = (<any[]>data).map(x => ({
+        name: x.name,
+        originalName: x.name,
+        questions: x.questions,
+        originalQuestionsChecksum: x.questions.map(x => x.name).join('~'),
+        markedForDelete: false
+      }));
+    }, (error) => {
+      console.log(error);
+      this.errorCallingRestEndpoint = true;
+    });
+  }
 
   setSelectedQuiz(q: QuizDisplay) {
     this.selectedQuiz = q;
