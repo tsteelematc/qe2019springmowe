e8ac5887de912a20d9282a6c99cb9ed396beb710
tsteelematc <tsteele@madisoncollege.edu>
2019-04-24 15:34:28 -0500

    I think it is working...

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -77,7 +77,11 @@ export class AppComponent implements OnInit {
         , question: x.questions
       }));
       
-    const addedQuizzes = [];
+    const addedQuizzes = this.getAddedQuizzes()
+      .map(x =>({
+        quizName: x.name
+        , quizQuestions: x.questions.map(x => x.name)
+      }));
 
     this.quizSvc.saveQuizzes(editedQuizzes, addedQuizzes).subscribe(
       numberOfEditedQuizzesSaved => console.log(numberOfEditedQuizzesSaved)
@@ -167,12 +171,16 @@ export class AppComponent implements OnInit {
     return this.getEditedQuizzes().length;
   }
 
-  get numberOfAddedQuizzes() {
+  getAddedQuizzes() {
     return this.quizzes
-    .filter(x => 
+      .filter(x => 
         !x.markedForDelete
         && x.originalName === 'New Untitled Quiz'
-    ).length;
+      );
+  }
+
+  get numberOfAddedQuizzes() {
+    return this.getAddedQuizzes().length;
   }
 
   //
