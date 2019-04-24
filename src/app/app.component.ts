aa6d7dd30720fd14e944d62fc1d1a45e8940c787
tsteelematc <tsteele@madisoncollege.edu>
2019-04-24 14:45:29 -0500

    Better Save Quizzes button badges...

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -136,14 +136,18 @@ export class AppComponent implements OnInit {
 
   get numberOfEditedQuizzes() {
     return this.quizzes
-    .filter(x => 
-      x.name != x.originalName
-      || x.originalQuestionsChecksum != x.questions.map(x => x.name).join('~')
-    ).length;
+      .filter(x => 
+        (!x.markedForDelete && x.originalName !== 'New Untitled Quiz')
+        && (x.name != x.originalName || x.originalQuestionsChecksum != x.questions.map(x => x.name).join('~'))
+      ).length;
   }
 
   get numberOfAddedQuizzes() {
-    return this.quizzes.filter(x => x.originalName === 'New Untitled Quiz').length;
+    return this.quizzes
+    .filter(x => 
+        !x.markedForDelete
+        && x.originalName === 'New Untitled Quiz'
+    ).length;
   }
 
   //
